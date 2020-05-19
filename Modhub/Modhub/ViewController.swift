ad338e970779401daa4c374220e5d5d8b2e37f45
Will Huynh <47924673+Will-huynh13@users.noreply.github.com>
2020-05-18 21:52:36 -0600

    Basic UI template, adding more features soon.

---
+++ Modhub/Modhub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Modhub
+//
+//  Created by Will Huynh on 5/18/20.
+//  Copyright © 2020 Will Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
