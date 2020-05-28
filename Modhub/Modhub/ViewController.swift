150d8266425e3413eda6edcfc80ab699d5e70b46
Will Huynh <47924673+Will-huynh13@users.noreply.github.com>
2020-05-27 18:11:36 -0600

    Added basic code to control the viewTable

--- Modhub/Modhub/ViewController.swift
+++ Modhub/Modhub/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController
-{
+// this
+var items: [String] = []
 
-    override func viewDidLoad()
-    {
+class ViewController: UIViewController {
+    // this is the table view on the main screen 
+    @IBOutlet weak var partsList: UITableView!
+
+    @IBOutlet weak var NumParts: UILabel! // this is the label for the number of parts
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        partsList.delegate = self
+        partsList.dataSource = self
+        NumParts.text = String(items.count) // counting the number of parts
+    }
+    
+}
+// end of UIViewController
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { // this function will handle user interaction when they select a cell
+        print(items)
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    // needs 2 func
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // this function will return the number of items in the array
+        return items.count
+    }
+    
+    // cell dequeueing function
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // this is just using the template over and over just replacing the data
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+}
